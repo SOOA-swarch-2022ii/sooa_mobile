c97e8e99aee050fb79f349faba7d19683e6c52ce
Danny Garzón <degarzonm@unal.edu.co>
2022-10-21 21:25:07 -0500

    continuation

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>aqui empieza el sooa</Text>
       <StatusBar style="auto" />
+      <Text style={styles.texto1}>Sooa Mobile</Text>
+      <Button title="log in" />
     </View>
   );
 }
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d9a002',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto1:{
+    Color: '#fff',
+  }
 });
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sooa_mobile
 Aplicacion React Native para SOOA
+
+# inicializar
+
+instala expo-cli: ```npm install -g expo-cli```
+ejecuta: ```npm start```
